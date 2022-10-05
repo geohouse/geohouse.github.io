73b760bbbaa4b00f5dfd3706e00e9fee03c7c7b3
Geoffrey House <geo.house7@gmail.com>
2022-10-05 17:26:57 -0600

    Working portfolio collapse POC with entries

--- js/projects-long-format.js
+++ js/projects-long-format.js
@@ -8,6 +8,9 @@ const detailsList = document.querySelectorAll(".details");
 collapseList.forEach((collapseItem, collapseItemIndex) => {
   collapseItem.addEventListener("click", (event) => {
     event.currentTarget.classList.toggle("active");
+    // Find the corresponding content for the collapse button through
+    // the index value that can be used to link the elements in the
+    // 2 NodeLists
     let collapseContent = detailsList[collapseItemIndex];
     if (collapseContent.style?.maxHeight) {
       collapseContent.style.maxHeight = null;
@@ -15,4 +18,34 @@ collapseList.forEach((collapseItem, collapseItemIndex) => {
       collapseContent.style.maxHeight = `${collapseContent.scrollHeight}px`;
     }
   });
+});
+
+// Provides the expand all and the collapse all functionality
+
+function updateCollapseState(action) {
+  if (action === "expand") {
+    collapseList.forEach((collapseItem, collapseItemIndex) => {
+      collapseItem.classList.add("active");
+      let collapseContent = detailsList[collapseItemIndex];
+      collapseContent.style.maxHeight = `${collapseContent.scrollHeight}px`;
+    });
+  } else {
+    collapseList.forEach((collapseItem, collapseItemIndex) => {
+      collapseItem.classList.remove("active");
+      let collapseContent = detailsList[collapseItemIndex];
+      collapseContent.style.maxHeight = null;
+    });
+  }
+}
+
+let expandAll = document.querySelectorAll(".expand-all");
+expandAll.forEach((expandButton) => {
+  expandButton.addEventListener("click", () => updateCollapseState("expand"));
+});
+
+let collapseAll = document.querySelectorAll(".collapse-all");
+collapseAll.forEach((collapseButton) => {
+  collapseButton.addEventListener("click", () =>
+    updateCollapseState("collapse")
+  );
 });
