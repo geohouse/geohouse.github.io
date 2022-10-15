b49a8fb203a0916eca1f570bb72ebdda94f686d3
Geoffrey House <geo.house7@gmail.com>
2022-10-15 17:51:28 -0600

    updating long format functionality

--- js/projects-long-format.js
+++ js/projects-long-format.js
@@ -5,9 +5,34 @@
 const collapseList = document.querySelectorAll(".collapse-button");
 const detailsList = document.querySelectorAll(".details");
 
+// Provides the expand all and the collapse all functionality
+
+function updateCollapseState(action) {
+  if (action === "expand") {
+    collapseList.forEach((collapseItem, collapseItemIndex) => {
+      collapseItem.classList.add("active");
+      let collapseContent = detailsList[collapseItemIndex];
+      collapseContent.style.maxHeight = `${collapseContent.scrollHeight}px`;
+      collapseItem.innerText = "-";
+    });
+  } else {
+    collapseList.forEach((collapseItem, collapseItemIndex) => {
+      collapseItem.classList.remove("active");
+      let collapseContent = detailsList[collapseItemIndex];
+      collapseContent.style.maxHeight = null;
+      collapseItem.innerText = "+";
+    });
+  }
+}
+
 collapseList.forEach((collapseItem, collapseItemIndex) => {
   collapseItem.addEventListener("click", (event) => {
     event.currentTarget.classList.toggle("active");
+    if (event.currentTarget.classList.contains("active")) {
+      event.currentTarget.innerText = "-";
+    } else {
+      event.currentTarget.innerText = "+";
+    }
     // Find the corresponding content for the collapse button through
     // the index value that can be used to link the elements in the
     // 2 NodeLists
@@ -18,25 +43,15 @@ collapseList.forEach((collapseItem, collapseItemIndex) => {
       collapseContent.style.maxHeight = `${collapseContent.scrollHeight}px`;
     }
   });
+  collapseItem.addEventListener("dblclick", (event) => {
+    console.log("double click");
+    if (event.currentTarget.classList.contains("active")) {
+      updateCollapseState("collapse");
+    } else {
+      updateCollapseState("expand");
+    }
+  });
 });
-
-// Provides the expand all and the collapse all functionality
-
-function updateCollapseState(action) {
-  if (action === "expand") {
-    collapseList.forEach((collapseItem, collapseItemIndex) => {
-      collapseItem.classList.add("active");
-      let collapseContent = detailsList[collapseItemIndex];
-      collapseContent.style.maxHeight = `${collapseContent.scrollHeight}px`;
-    });
-  } else {
-    collapseList.forEach((collapseItem, collapseItemIndex) => {
-      collapseItem.classList.remove("active");
-      let collapseContent = detailsList[collapseItemIndex];
-      collapseContent.style.maxHeight = null;
-    });
-  }
-}
 
 let expandAll = document.querySelectorAll(".expand-all");
 expandAll.forEach((expandButton) => {
