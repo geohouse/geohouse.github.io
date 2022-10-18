8d67260d61d39da0aa763bb53c5d8732407c751f
Geoffrey House <geo.house7@gmail.com>
2022-10-17 21:11:37 -0600

    project filtering display

--- js/main.js
+++ js/main.js
@@ -1,5 +1,64 @@
+const allButton = document.querySelector("#all");
+const gisButton = document.querySelector("#gis");
+const bioResearchButton = document.querySelector("#biologyresearch");
+const jsButton = document.querySelector("#javascript");
+const rButton = document.querySelector("#rscript");
+const pyButton = document.querySelector("#python");
+const bashButton = document.querySelector("#bash");
+const leafletButton = document.querySelector("#leaflet");
+const mapboxButton = document.querySelector("#mapbox");
+const rshinyButton = document.querySelector("#rshiny");
+
+const allProjects = document.querySelectorAll(".column");
+
+// Shows all by default. Filtering adds a .hide class to
+// projects NOT meant to be shown, keeping the others visible.
+function filterProjects(projectClass) {
+  console.log(projectClass);
+  allProjects.forEach((project) => {
+    if (projectClass === "all") {
+      project.classList.remove("hide");
+    } else if (project.classList.contains(projectClass)) {
+      project.classList.remove("hide");
+    } else {
+      project.classList.add("hide");
+    }
+  });
+}
+
+allButton.addEventListener("click", () => {
+  filterProjects("all");
+});
+gisButton.addEventListener("click", () => {
+  filterProjects("gis");
+});
+bioResearchButton.addEventListener("click", () => {
+  filterProjects("biologyresearch");
+});
+jsButton.addEventListener("click", () => {
+  filterProjects("javascript");
+});
+rButton.addEventListener("click", () => {
+  filterProjects("rscript");
+});
+pyButton.addEventListener("click", () => {
+  filterProjects("python");
+});
+bashButton.addEventListener("click", () => {
+  filterProjects("bash");
+});
+leafletButton.addEventListener("click", () => {
+  filterProjects("leaflet");
+});
+mapboxButton.addEventListener("click", () => {
+  filterProjects("mapbox");
+});
+rshinyButton.addEventListener("click", () => {
+  filterProjects("rshiny");
+});
+
 // From https://www.w3schools.com/howto/howto_js_portfolio_filter.asp
-filterSelection("all")
+filterSelection("all");
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("column");
@@ -15,7 +74,9 @@ function w3AddClass(element, name) {
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
   }
 }
 
@@ -25,18 +86,17 @@ function w3RemoveClass(element, name) {
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
     while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
     }
   }
   element.className = arr1.join(" ");
 }
 
-
 // Add active class to the current button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
+  btns[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
